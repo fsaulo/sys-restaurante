9a8e538d91b2b35bac3cfb7c54e27c4e165b4664
fsaulo <meetnapoleon@protonmail.ch>
2020-02-03 18:43:00 -0300

    Minor table tiles embellishments.

--- src/org/sysRestaurante/gui/ManageComandaController.java
+++ src/org/sysRestaurante/gui/ManageComandaController.java
@@ -2,12 +2,17 @@ package org.sysRestaurante.gui;
 
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.Separator;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.TilePane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
 import org.sysRestaurante.applet.AppFactory;
 import org.sysRestaurante.dao.ComandaDao;
 import org.sysRestaurante.gui.formatter.CurrencyField;
@@ -42,10 +47,22 @@ public class ManageComandaController {
                 Label label1 = new Label("Comanda #" + item.getIdComanda());
                 Label label2 = new Label("Ocupado");
                 Label label3 = new Label(CurrencyField.getBRLCurrencyFormat().format(item.getTotal()));
-                vbox.getChildren().addAll(label1, label2, label3);
+                Label label4 = new Label("MESA #" + item.getIdTable());
+                label1.setStyle("-fx-font: Carlito 15; -fx-font-weight: bold");
+                label2.setStyle("-fx-font: Carlito 15; -fx-font-weight: bold");
+                label3.setStyle("-fx-font: Carlito 15; -fx-font-weight: bold");
+                label4.setStyle("-fx-font: Carlito 17; -fx-font-weight: bold");
+                Circle icon = new Circle(4);
+                icon.setFill(Color.ORANGE);
+                label2.setGraphic(icon);
+                Separator sep = new Separator();
+                sep.setMinHeight(3);
+                vbox.getChildren().addAll(label4, sep, label1, label2, label3);
                 vbox.setStyle("-fx-background-color: #dddddd; -fx-border-color: black; -fx-cursor: hand");
                 vbox.setSpacing(3);
                 vbox.setPadding(new Insets(3));
+                vbox.setAlignment(Pos.TOP_RIGHT);
+                vbox.setMinWidth(150);
                 vbox.setOnMouseClicked(event -> {
                     Alert alert = new Alert(Alert.AlertType.INFORMATION);
                     alert.setTitle("Informação do sistema");
@@ -54,6 +71,25 @@ public class ManageComandaController {
                     alert.initOwner(vbox.getScene().getWindow());
                     alert.showAndWait();
                 });
+
+                vbox.setOnMouseEntered(event -> {
+                    vbox.setStyle("-fx-background-color: gray; -fx-border-color: black; -fx-cursor: hand");
+                    label1.setStyle("-fx-font: Carlito 15; -fx-font-weight: bold; -fx-text-fill: white");
+                    label2.setStyle("-fx-font: Carlito 15; -fx-font-weight: bold; -fx-text-fill: white");
+                    label3.setStyle("-fx-font: Carlito 15; -fx-font-weight: bold; -fx-text-fill: white");
+                    label4.setStyle("-fx-font: Carlito 15; -fx-font-weight: bold; -fx-text-fill: white");
+                });
+
+                vbox.setOnMouseExited(event -> {
+                    vbox.setStyle("-fx-background-color: #dddddd; -fx-border-color: black; -fx-cursor: hand");
+                    label1.setStyle("-fx-font: Carlito 15; -fx-font-weight: bold; -fx-text-fill: black");
+                    label2.setStyle("-fx-font: Carlito 15; -fx-font-weight: bold; -fx-text-fill: black");
+                    label3.setStyle("-fx-font: Carlito 15; -fx-font-weight: bold; -fx-text-fill: black");
+                    label4.setStyle("-fx-font: Carlito 15; -fx-font-weight: bold; -fx-text-fill: black");
+                });
+
+                icon.setOnMouseEntered(event -> icon.setRadius(6));
+                icon.setOnMouseExited(event -> icon.setRadius(4));
                 tilePane.getChildren().addAll(vbox);
             }
         }
