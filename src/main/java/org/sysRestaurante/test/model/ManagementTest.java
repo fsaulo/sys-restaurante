3e2d317a99cf8af5ab69cdfcb6f0edfa3d2baef4
Saulo G. Felix <meetnapoleon@protonmail.ch>
2021-08-19 01:03:22 -0300

    [Test] add tests for metadata socket

--- src/main/java/org/sysRestaurante/test/model/ManagementTest.java
+++ src/main/java/org/sysRestaurante/test/model/ManagementTest.java
@@ -6,29 +6,42 @@ import org.sysRestaurante.model.Management;
 
 import java.util.Random;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class ManagementTest {
 
+    MetadataDao businessInfo;
+    int id = 0;
+
     @Test
     void shouldGetBusinessInfo() {
-        MetadataDao businessInfo = Management.getBusinessInfo();
+        this.businessInfo = Management.getBusinessInfo();
         assertNotNull(businessInfo);
     }
 
     @Test
     void shouldInsertMetadata() {
-        Management.setMetadata(new Random().nextInt());
+        Management.setMetadata(id);
     }
 
     @Test
     void shouldUpdateBusinessInfo() {
-        MetadataDao businessInfo = new MetadataDao();
-        businessInfo.setId(1);
-        businessInfo.setBusinessAddress("String: Adress");
-        businessInfo.setBusinessName("String: Business Name");
-        businessInfo.setBusinessPhone("String: Phone");
-        businessInfo.setBusinessCNPJ("String: CNPJ");
-        Management.updateBusinessInfo(businessInfo);
+        MetadataDao info = new MetadataDao();
+        assertNotNull(info);
+
+        info.setId(id);
+        info.setBusinessAddress("String: Adress");
+        info.setBusinessName("String: Business Name");
+        info.setBusinessPhone("String: Phone");
+        info.setBusinessCNPJ("String: CNPJ");
+
+        Management.updateBusinessInfo(info);
+        MetadataDao updatedInfo = Management.getBusinessInfo();
+
+        assertEquals(updatedInfo.getBusinessAddress(), "String: Adress");
+        assertEquals(updatedInfo.getBusinessName(), "String: Business Name");
+        assertEquals(updatedInfo.getBusinessPhone(), "String: Phone");
+        assertEquals(updatedInfo.getBusinessCNPJ(), "String: CNPJ");
     }
 }
