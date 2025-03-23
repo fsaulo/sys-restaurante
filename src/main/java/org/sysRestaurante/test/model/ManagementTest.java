5b1a622d6fc26addaeeec5cf5b71b7b3f5ae4b42
Saulo G. Felix <sgf3lix@gmail.com>
2025-03-22 21:03:37 -0300

    Add Management table to the database

--- src/main/java/org/sysRestaurante/test/model/ManagementTest.java
+++ src/main/java/org/sysRestaurante/test/model/ManagementTest.java
@@ -4,43 +4,67 @@ import org.junit.jupiter.api.Test;
 import org.sysRestaurante.dao.MetadataDao;
 import org.sysRestaurante.model.Management;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 class ManagementTest {
 
     MetadataDao businessInfo;
     int id = 0;
+    String address = "String: Address";
+    String name = "String: Business Name";
+    String phone = "String: Phone";
+    String cnpj = "String: CNPJ";
+
+    MetadataDao createBusinessInfo() {
+        MetadataDao info = new MetadataDao();
+        info.setId(id);
+        info.setBusinessAddress(address);
+        info.setBusinessName(name);
+        info.setBusinessPhone(phone);
+        info.setBusinessCNPJ(cnpj);
+        return info;
+    }
 
     @Test
     void shouldGetBusinessInfo() {
-        this.businessInfo = Management.getBusinessInfo();
+        businessInfo = Management.getBusinessInfo(id);
         assertNotNull(businessInfo);
     }
 
     @Test
     void shouldInsertMetadata() {
-        Management.setMetadata(id);
+        MetadataDao info = createBusinessInfo();
+
+        assertNotNull(info);
+        Management.updateBusinessInfo(info);
     }
 
     @Test
     void shouldUpdateBusinessInfo() {
-        MetadataDao info = new MetadataDao();
+        MetadataDao info = createBusinessInfo();
         assertNotNull(info);
 
-        info.setId(id);
-        info.setBusinessAddress("String: Adress");
-        info.setBusinessName("String: Business Name");
-        info.setBusinessPhone("String: Phone");
-        info.setBusinessCNPJ("String: CNPJ");
-
         Management.updateBusinessInfo(info);
-        MetadataDao updatedInfo = Management.getBusinessInfo();
+        MetadataDao updatedInfo = Management.getBusinessInfo(id);
 
-        assert updatedInfo != null;
-        assertEquals(updatedInfo.getBusinessAddress(), "String: Adress");
-        assertEquals(updatedInfo.getBusinessName(), "String: Business Name");
-        assertEquals(updatedInfo.getBusinessPhone(), "String: Phone");
-        assertEquals(updatedInfo.getBusinessCNPJ(), "String: CNPJ");
+        assertNotNull(updatedInfo);
+        assertEquals(address, updatedInfo.getBusinessAddress());
+        assertEquals(name, updatedInfo.getBusinessName());
+        assertEquals(phone, updatedInfo.getBusinessPhone());
+        assertEquals(cnpj, updatedInfo.getBusinessCNPJ());
+    }
+
+    @Test
+    void shouldDeleteBusinessInfo() {
+        MetadataDao info = createBusinessInfo();
+        assertNotNull(info);
+        Management.updateBusinessInfo(info);
+        Management.deleteMetadata(info.getId());
+        MetadataDao emptyInfo = Management.getBusinessInfo(info.getId());
+        assertNotNull(emptyInfo);
+        assertNull(emptyInfo.getBusinessAddress());
+        assertNull(emptyInfo.getBusinessName());
+        assertNull(emptyInfo.getBusinessPhone());
+        assertEquals(0, emptyInfo.getId());
     }
 }
