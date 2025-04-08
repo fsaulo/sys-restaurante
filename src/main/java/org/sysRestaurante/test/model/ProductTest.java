b5293d65b19d2d183a2d6983d301ab53c155d92d
Saulo G. Felix <sgf3lix@gmail.com>
2025-04-07 22:34:36 -0300

    Add ProductTests

--- src/main/java/org/sysRestaurante/test/model/ProductTest.java
+++ src/main/java/org/sysRestaurante/test/model/ProductTest.java
@@ -1,4 +1,46 @@
 package org.sysRestaurante.test.model;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.sysRestaurante.dao.ProductDao;
+import org.sysRestaurante.model.Product;
+
 public class ProductTest {
+
+    private static int productId = 0;
+
+    public ProductDao createTestProduct() {
+        ProductDao product = new ProductDao();
+        product.setDescription("Test");
+        product.setCategoryDao(new ProductDao.CategoryDao());
+        product.setIdCategory(0);
+        return product;
+    }
+
+    @Test
+    public void shouldInsertProduct() {
+        ProductDao product = createTestProduct();
+        assertNotNull(product);
+        productId = Product.insert(product);
+        assertNotEquals(-1, productId);
+    }
+    
+    @Test
+    public void shouldUpdateProduct() {
+        ProductDao product = createTestProduct();
+        assertNotNull(product);
+
+        product.setIdProduct(productId);
+        product.setDescription("Test2");
+        Product.update(product);
+    }
+
+    @Test
+    public void shouldRemoveProduct() {
+        ProductDao product = createTestProduct();
+        assertNotNull(product);
+
+        product.setIdProduct(productId);
+        Product.remove(product.getIdProduct());
+    }
 }
